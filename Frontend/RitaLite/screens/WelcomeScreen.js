f1c8a4c0c4c59d1ec2f6432e51ff8b7c900b00f2
Abdul <abdulaziz.ju.100@gmail.com>
2024-11-27 19:37:46 +0000

    update screen

--- Frontend/RitaLite/screens/WelcomeScreen.js
+++ Frontend/RitaLite/screens/WelcomeScreen.js
@@ -6,7 +6,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 function WelcomeScreen({ navigation }) {
   return (
     <LinearGradient
-      colors={['#38B5FE', '#9641FF']} // need to change this to Rita color
+      colors={['#9641FF', '#38B5FE' ]} // need to change this to Rita color
       style={styles.container}
     >
     <View style={styles.container}>
@@ -16,17 +16,19 @@ function WelcomeScreen({ navigation }) {
         resizeMode="contain"
       />
       <Text style={styles.title}>Your digital healthcare assistant</Text>
+      <Text style={styles.subtitle}>Log medication, timing, doses, get 
+notification, and track your medication
+history</Text>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('IntroScreen')}
       >
-        <Text style={styles.buttonText}>Let’s get started</Text>
+        <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.link}
         onPress={() => navigation.navigate('LoginScreen')}
       >
-        <Text style={styles.linkText}>I already have an account</Text>
       </TouchableOpacity>
     </View>
     </LinearGradient>
@@ -34,13 +36,30 @@ function WelcomeScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center'},
-  logo: {width: 250, height: 250, marginBottom: 10}, 
-  title: { fontSize: 34, color: 'white', marginBottom: 20, fontWeight: 'bold', marginTop: 20, textAlign: 'center'},
-  button: { backgroundColor: '#0F4D80', padding: 20, borderRadius: 40, marginTop: 50, alignItems: 'center', width: 350},
-  buttonText: { color: 'white', fontSize: 24, fontWeight: 'bold'},
-  link: { marginTop: 10 },
-  linkText: { color: 'white', fontSize: 20, textDecorationLine: 'underline' },
+  container: 
+  { flex: 1, justifyContent: 'center', alignItems: 'center'},
+  
+  logo: 
+  {width: 214.56, height: 100, marginBottom: 40, marginTop: 80}, 
+  
+  title: 
+  { fontSize: 48, color: 'white', marginBottom: 71, fontWeight: 'bold', marginTop: 20, textAlign: 'center'},
+
+  subtitle: 
+  { fontSize: 18, color: 'white', marginBottom: 47, fontWeight: 'bold', marginTop: 10, textAlign: 'center', lineHeight: 24},
+  
+  button: 
+  { backgroundColor: '#0F4D80', padding: 10, borderRadius: 45, marginTop: 107, alignItems: 'center', width: 308, height: 55},
+  
+  buttonText: 
+  { color: 'white', fontSize: 24, fontWeight: 'bold'},
+  
+  link: 
+  { marginTop: 10 },
+  
+  linkText: 
+  { color: 'white', fontSize: 20, textDecorationLine: 'underline' },
+
 });
 
 export default WelcomeScreen;
